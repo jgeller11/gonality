5019e4e8de1d1b13926319da824e1f56c5f32c0f
jgeller11 <34071775+jgeller11@users.noreply.github.com>
2021-12-10 20:50:07 -0500

    made a small change to the process of checking whether or not a divisor wins the gonality game, and added a method to generate random graphs

--- javaImplementation/Gonality.java
+++ javaImplementation/Gonality.java
@@ -3,28 +3,30 @@ package javaImplementation;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Iterator;
+import java.lang.Math; 
 
 class Gonality {
     public static void main(String[] args) {
         System.out.println("running");
         Gonality g = new Gonality();
+
+        
+
+
         //P3xP3xP3: 9
         //P3xP3xP4: 9
         //P3xP3xP5: 9
         //P3xP3xC3: 9
         //P3xC3xC3: 9
         //
-      
-
-        Graph graph = Graph.cycle(3).product(Graph.cycle(3).product(Graph.cycle(3)));
-
+        // Graph graph = Graph.cycle(3).product(Graph.cycle(3));
+        Graph graph =  new Graph(harary(19,3));
+        
         double startTime = System.currentTimeMillis();
         System.out.println(g.gonality(graph, 14, true));
         double time = System.currentTimeMillis() - startTime;
         System.out.println("ran in " + time / 1000 + " s");
 
-
-        //square.printAdjList();
     }
 
     boolean checkQReducedWinnable(Graph g, int[] divisor, int i, boolean[] pos) {
@@ -93,6 +95,29 @@ class Gonality {
         return output;
     }
 
+    // generates a random simple graph's adjacency list on n vertices with m edges, (n)(n-1)/2 > m > n 
+    // begins with a cycle to ensure graph is connected, then adds m-n more edges
+    static int[][] randomGraph(int n, int m){
+        int[][] output = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            output[i][(i+1)%n]=1;
+            output[(i+1)%n][i]=1;
+        }
+        for (int i = 0; i < (m-n); i++) {
+            boolean edgeBuilt = false;
+            while (!edgeBuilt){
+                int v1 = (int) (Math.random() * n);
+                int v2 = (int) (Math.random() * n);
+                if ((v1!=v2)&&(output[v1][v2]==0)){
+                    output[v1][v2]=1;
+                    output[v2][v1]=1;
+                    edgeBuilt = true;
+                }
+            }
+        }
+        return output;
+    }
+
     static int[][] gridGraph(int n1, int n2, int n3) {
         int[][] output = new int[n1*n2*n3][n1*n2*n3];
         for(int i = 0; i < n1*n2*n3; i++){
@@ -138,12 +163,15 @@ class Gonality {
         int count = 0;
         Iterator<int[]> d;
         while (true) {
+            
             System.out.println(deg);
+            
             if (sym){
                 d = new SymmetryDegreeIterator(deg, n, degreeList);
             } else {
                 d = new DegreeIterator(deg, n, degreeList);
             }
+            int i = 1;
             while (d.hasNext()) {
                 // optimization?: order vertices in graph from max to min degree
                 count++;
@@ -162,7 +190,11 @@ class Gonality {
                         pos[m] = true;
                     }
                 }
-                for (int i = 1; i < n; i++) {
+                // int last = (n+i-1)%n;
+                
+                int last = ((n+i-3)%(n-1))+1;
+                while (i!= last){ 
+                // for (int i = 1; i<n; i++){
                     if ((!pos[i]) && (divisorCopy[i] == 0)) {
 
                         if (!checkQReducedWinnable(g, divisorCopy, i, pos)) {
@@ -170,6 +202,9 @@ class Gonality {
                             break;
                         }
                     }
+                    // i=(i+1)%n;
+
+                    i = ((i)%(n-1))+1;
                 }
                 if (found) {
                     System.out.println(Arrays.toString(currentDivisor));
